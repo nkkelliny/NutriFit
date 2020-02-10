262d1da92dad7db96bfa52aedacb4f9143b4cd5c
nkelliny1 <nkelliny@outlook.com>
2020-02-09 21:39:33 -0500

    Changes to add authentication and finish up api usage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,96 @@
 
   $(document).ready(function(){
     $('.modal').modal();
+    $('select').formSelect();
   });
 
-  $('.dropdown-trigger').dropdown();
+  var userName = document.getElementById('email1');
+  var pw = document.getElementById('password1');
+  
+  function store() {
+      localStorage.setItem('name', userName.value);
+      localStorage.setItem('pw', pw.value);
+      window.location.href = 'https://nkelliny1.github.io/NutriFit/map.html';
+  }
+  
+  function check() {
+  
+      var storedName = localStorage.getItem('name');
+      var storedPw = localStorage.getItem('pw');
+  
+      var userName = document.getElementById('email');
+      var userPw = document.getElementById('password');
+  
+      if(userName.value !== storedName || userPw.value !== storedPw) {
+          console.log('ERROR');
 
-  $("#password1").on("focusout", function (e) {
-    if ($(this).val() != $("#passwordConfirm1").val()) {
-        $("#passwordConfirm1").removeClass("valid").addClass("invalid");
-    } else {
-        $("#passwordConfirm1").removeClass("invalid").addClass("valid");
-    }
-});
+      }else {
+          console.log('You are loged in.');
+          window.location.href = 'https://nkelliny1.github.io/NutriFit/map.html';
+      }
+  }
 
-$("#passwordConfirm1").on("keyup", function (e) {
-    if ($("#password1").val() != $(this).val()) {
-        $(this).removeClass("valid").addClass("invalid");
-    } else {
-        $(this).removeClass("invalid").addClass("valid");
-    }
-});
+var recipes = ['2 granola bars', 'whole grain cereal', '2 eggs', '1 bagel with 1 tbsp cream cheese']
 
-var auth =  [
-    { email: "email@email.com" , password: "Hello"},
-]
+$( "select" )
+  .change(function () {
+    var str = "";
+    $( "select option:selected" ).each(function() {
+      str = $( this ).val();
+    });
+    console.log(str);
 
-function loginVerification(){
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-}
+    document.getElementById('mealText').value = recipes[str];
+
+    var meal = "bread";
+    var mealImg = '';
+    var mealText = '';
+    $.ajax({
+        type: "GET",
+        url: "https://trackapi.nutritionix.com/v2/search/instant?query=" + recipes[str] + " healthy" + "&self=true&branded=true&common=true&common_general=true&common_grocery=true&common_restaurant=true&detailed=false&claims=false",
+    headers: {
+            'x-app-id': '17b9542d',   //If your header name has spaces or any other char not appropriate
+            'x-app-key': 'baf2162fd62075e1574fc8922466bac9'  //for object property name, use quoted notation shown in second
+        },
+        dataType: 'json',
+        success: function (data) {
+            console.info(data);
+            mealImg = data.common[0].photo.thumb;
+            mealText = data.common[0].food_name;
+            console.log(mealImg);
+            $('#mealImg').attr("src",mealImg);
+            $('#mealText').attr("value", mealText);
+
+            $('#nlabel').nutritionLabel({
+                showServingUnitQuantity : false,
+                itemName : str,
+                ingredientList : str,
+            
+                showPolyFat : false,
+                showMonoFat : false,
+            
+                valueCalories : 450,
+                valueFatCalories : 430,
+                valueTotalFat : 48,
+                valueSatFat : 6,
+                valueTransFat : 0,
+                valueCholesterol : 30,
+                valueSodium : 780,
+                valueTotalCarb : 3,
+                valueFibers : 0,
+                valueSugars : 3,
+                valueProteins : 3,
+                valueVitaminD : 12.22,
+                valuePotassium_2018 : 4.22,
+                valueCalcium : 7.22,
+                valueIron : 11.22,
+                valueCaffeine : 15.63,
+                showLegacyVersion : false
+            });
+        }
+    });
+  })
+  .change();
 
 var meal = "bread";
 var mealImg = '';
@@ -88,3 +150,15 @@ setTimeout(function(){
           $('#pac-input').trigger(e);
 }, 3000);
 
+// $.ajax({
+//     type: "POST",
+//     url: "https://trackapi.nutritionix.com/v2/natural/nutrients?query=butter",
+//     headers: {
+//         'x-app-id': '17b9542d',   //If your header name has spaces or any other char not appropriate
+//         'x-app-key': 'baf2162fd62075e1574fc8922466bac9',  //for object property name, use quoted notation shown in second
+//     },
+//     dataType: 'json',
+//     success: function (data) {
+//         console.info(data);
+//     }
+// });
